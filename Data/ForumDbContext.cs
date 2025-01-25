3d23af739e0af0f7d2f5d3851ec77d67515ad7df
HaiderAliCoder <haiderali.47007@gmail.com>
2025-01-25 12:55:38 +0500

    Add project files.

---
+++ Data/ForumDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestInifniteScroll.Models;
+namespace TestInifniteScroll.Data
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(80);
+            });
+        }
+    }
+
+}
---
+++ Data/ItemRepository.cs
@@ -0,0 +1,28 @@
+﻿using TestInifniteScroll.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+
+namespace TestInifniteScroll.Data
+{
+    public class ItemRepository
+    {
+        private readonly ForumDbContext _context;
+
+        public ItemRepository(ForumDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Item>> GetItemsAsync(int offset, int pageSize)
+        {
+            var items = await _context.Items
+        .FromSqlRaw("EXEC GetItems @Offset = {0}, @PageSize = {1}", offset, pageSize)
+        .ToListAsync();
+            return items;
+        }
+    }
+}
